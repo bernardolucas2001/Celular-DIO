04721594298972313011d13f025d95c33595c006
Bernardo Lucas de Sousa <bernardosaopaulino@gmail.com>
2023-12-15 22:54:28 -0300

    Desafio realizado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using CelularDIO.Models;
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "654321", modelo: "Modelo 1", imei: "1111111111", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "8000", modelo: "Modelo 2", imei: "2222222222", memoria: 256);
+iphone.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
